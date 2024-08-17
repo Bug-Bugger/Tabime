f024e2d868ab296f2c3f6d867c7906ea63125372
Huajie Zhong <hz642@cornell.edu>
2024-08-17 17:50:42 -0400

    add feature to hide table

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,11 +2,10 @@ import { useState } from "react";
 
 export default function Landing() {
   const [sortedXml, setSortedXml] = useState("");
-  const [fileName, setFileName] = useState("");
+  const [fileUploaded, setFileUploaded] = useState(false);
 
   const handleFileUpload = (event) => {
     const file = event.target.files[0];
-    setFileName(file.name);
 
     const reader = new FileReader();
     reader.onload = (e) => {
@@ -45,6 +44,7 @@ export default function Landing() {
     }
 
     const tableHtml = jsonToTable(jsonAnimes);
+    setFileUploaded(true);
     setSortedXml(tableHtml);
   };
 
@@ -54,17 +54,19 @@ export default function Landing() {
         <h1 className="text-3xl font-sans font-semibold">OtakuOdyssey</h1>
       </div>
       <div className="flex justify-center">
-        <form className="w-1/2 p-6">
-          <h1 className="block font-medium text-gray-300">
-            Upload list file (XML)
-          </h1>
-          <input
-            type="file"
-            accept=".xml"
-            className="mt-1 block px-3 py-5 w-fit"
-            onChange={handleFileUpload}
-          />
-        </form>
+        {!fileUploaded && (
+          <form className="w-1/2 p-6">
+            <h1 className="block font-medium text-gray-300">
+              Upload list file (XML)
+            </h1>
+            <input
+              type="file"
+              accept=".xml"
+              className="mt-1 block px-3 py-5 w-fit"
+              onChange={handleFileUpload}
+            />
+          </form>
+        )}
       </div>
 
       <div
