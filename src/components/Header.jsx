237e418d83304375034716284b943bdf6b33f28d
Huajie Zhong <hz642@cornell.edu>
2024-11-26 17:04:36 -0500

    fix z index

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <div className="h-auto fixed w-full">
+    <div className="h-auto fixed w-full z-40">
       <div className="p-6 mx-auto">
         <h1 className="text-3xl font-sans font-semibold">Tabime</h1>
       </div>
