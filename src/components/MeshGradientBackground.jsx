75001ac10a387aa346c07e6b2eb02fee4f81d39c
Huajie Zhong <hz642@cornell.edu>
2024-11-26 21:50:44 -0500

    fix randomization

--- src/components/MeshGradientBackground.jsx
+++ src/components/MeshGradientBackground.jsx
@@ -2,54 +2,58 @@ import { useRef, useEffect } from "react";
 import gsap from "gsap";
 
 const MeshGradientBackground = () => {
-  const bg = useRef(null);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      generateRandomGradient();
-    }, 1000);
-    const generateRandomGradient = () => `
-      radial-gradient(at ${gsap.utils.random(0, 79)}% ${gsap.utils.random(
-      30,
-      100
-    )}%, 
-        hsla(${gsap.utils.random(185, 215)},77%,64%,1) 0px, transparent 50%),
-      radial-gradient(at ${gsap.utils.random(30, 100)}% ${gsap.utils.random(
-      0,
-      60
-    )}%, 
-        hsla(${gsap.utils.random(208, 238)},76%,63%,1) 0px, transparent 50%),
+  const generateRandomGradient = () => `
+      radial-gradient(at ${gsap.utils.random(10, 90)}% ${gsap.utils.random(
+    10,
+    90
+  )}%, 
+        hsla(${gsap.utils.random(185, 215)},${gsap.utils.random(
+    60,
+    80
+  )}%,${gsap.utils.random(60, 80)}%,1) 0px, transparent 70%),
+      radial-gradient(at ${gsap.utils.random(10, 90)}% ${gsap.utils.random(
+    0,
+    60
+  )}%, 
+        hsla(${gsap.utils.random(208, 238)},${gsap.utils.random(
+    60,
+    80
+  )}%,${gsap.utils.random(60, 80)}%,1) 0px, transparent 70%),
       radial-gradient(at ${gsap.utils.random(0, 100)}% ${gsap.utils.random(
-      0,
-      100
-    )}%, 
-        hsla(${gsap.utils.random(185, 215)},68%,75%,1) 0px, transparent 50%),
+    0,
+    100
+  )}%, 
+        hsla(${gsap.utils.random(185, 215)},${gsap.utils.random(
+    60,
+    80
+  )}%,${gsap.utils.random(60, 80)}%,1) 0px, transparent 70%),
       radial-gradient(at ${gsap.utils.random(0, 100)}% ${gsap.utils.random(
-      0,
-      100
-    )}%, 
-        hsla(${gsap.utils.random(246, 276)},54%,60%,1) 0px, transparent 50%)
+    0,
+    100
+  )}%, 
+        hsla(${gsap.utils.random(256, 276)},${gsap.utils.random(
+    40,
+    80
+  )}%,${gsap.utils.random(60, 80)}%,1) 0px, transparent 70%)
     `;
 
-    gsap.fromTo(
-      bg.current,
-      {
-        backgroundColor: "hsla(228,39%,82%,1)",
-        backgroundImage: generateRandomGradient(),
-      },
-      {
-        backgroundImage: generateRandomGradient(),
-        duration: 4,
-        ease: "power1.inOut",
-        repeat: -1,
-        yoyo: true,
-        onRepeat: function () {
-          this.vars.backgroundImage = generateRandomGradient();
-        },
-      }
-    );
+  const bg = useRef(null);
 
-    return () => clearInterval(interval);
+  const randomMovement = () => {
+    gsap.to(bg.current, {
+      backgroundImage: generateRandomGradient(),
+      duration: 1.5,
+      ease: "linear",
+      onComplete: randomMovement,
+    });
+  };
+
+  useEffect(() => {
+    gsap.set(bg.current, {
+      backgroundColor: "hsla(228,39%,82%,1)",
+      backgroundImage: generateRandomGradient(),
+    });
+    randomMovement();
   }, []);
 
   return <div ref={bg} className="h-screen gradient"></div>;
