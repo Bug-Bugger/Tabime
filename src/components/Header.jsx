6bc1a08e507efb67597f629fea34e7d1399004dc
Huajie Zhong <hz642@cornell.edu>
2024-11-28 01:18:27 -0500

    add animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,11 @@ export default function Header() {
   return (
     <div className="h-auto fixed w-full z-40">
       <div className="p-6 mx-auto flex items-center">
-        <img src={logo} alt="Tabime" className="w-12 h-12" />
+        <img
+          src={logo}
+          alt="Tabime"
+          className="w-12 h-12 hover:rotate-180 ease-in-out transition-all duration-500"
+        />
         <div className="text-3xl font-sans font-semibold ml-2">Tabime</div>
       </div>
     </div>
