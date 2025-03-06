20c9d5cecafb64026257fb31319e040199bae264
Huajie Zhong <hz642@cornell.edu>
2025-03-06 00:44:56 -0500

    improve description

--- src/components/Pages/description.tsx
+++ src/components/Pages/description.tsx
@@ -10,8 +10,6 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import Image from "next/image";
 
-gsap.registerPlugin(ScrollTrigger);
-
 const Description: React.FC = () => {
   const containerRef = useRef<HTMLDivElement>(null);
 
@@ -30,137 +28,140 @@ const Description: React.FC = () => {
   const spRef = useRef<SVGSVGElement>(null);
 
   useEffect(() => {
-    const tl = gsap.timeline({
-      defaults: { ease: "power2.inOut" },
-      scrollTrigger: {
-        trigger: containerRef.current,
-        start: "top top",
-        end: "+=3600 bottom",
-        scrub: 2,
-        pin: true,
-        snap: {
-          snapTo: "labelsDirectional",
-          duration: 0.4,
-          delay: 0.1,
-          ease: "power1.inOut",
-          inertia: false,
+    gsap.registerPlugin(ScrollTrigger);
+
+    const ctx = gsap.context(() => {
+      const tl = gsap.timeline({
+        defaults: { ease: "power2.inOut" },
+        scrollTrigger: {
+          trigger: containerRef.current,
+          start: "top top",
+          end: "+=3600 bottom",
+          scrub: 2,
+          pin: true,
+          snap: {
+            snapTo: "labelsDirectional",
+            duration: 0.4,
+            delay: 0.1,
+            ease: "power1.inOut",
+            inertia: false,
+          },
         },
-      },
-    });
+      });
 
-    // It seems that gsap transformOrigin is not
-    // working well with translating the element
-    // Thus this offset function is used to calculate
-    interface Offset {
-      top: string;
-      left: string;
-    }
+      // It seems that gsap transformOrigin is not
+      // working well with translating the element
+      // Thus this offset function is used to calculate
+      interface Offset {
+        top: string;
+        left: string;
+      }
 
-    const offset = (top: number, left: number): Offset => ({
-      top: `${top + 6}%`,
-      left: `${left - 2}%`,
-    });
+      const offset = (top: number, left: number): Offset => ({
+        top: `${top + 6}%`,
+        left: `${left - 2}%`,
+      });
 
-    // The reveal segment by legnth require calculation of the path
-    // Thus this is the function for it
-    interface PathStrokeDash {
-      strokeDasharray: number;
-      strokeDashoffset: number;
-    }
+      // The reveal segment by legnth require calculation of the path
+      // Thus this is the function for it
+      interface PathStrokeDash {
+        strokeDasharray: number;
+        strokeDashoffset: number;
+      }
 
-    function revealSegmentByLength(
-      path: SVGPathElement | null
-    ): PathStrokeDash {
-      const length: number = path?.getTotalLength() ?? 0;
-      const svg: SVGSVGElement | null = trivialPathSvgRef.current;
+      function revealSegmentByLength(
+        path: SVGPathElement | null
+      ): PathStrokeDash {
+        const length: number = path?.getTotalLength() ?? 0;
+        const svg: SVGSVGElement | null = trivialPathSvgRef.current;
 
-      const scale: number = svg
-        ? svg.clientWidth / svg.viewBox.baseVal.width
-        : 1;
+        const scale: number = svg
+          ? svg.clientWidth / svg.viewBox.baseVal.width
+          : 1;
 
-      const trueLength: number = length * scale;
-      return {
-        strokeDasharray: trueLength,
-        strokeDashoffset: trueLength,
-      };
-    }
+        const trueLength: number = length * scale;
+        return {
+          strokeDasharray: trueLength,
+          strokeDashoffset: trueLength,
+        };
+      }
 
-    tl.set(collabPointerRef.current, { opacity: 0 })
-      .to(pointerRef.current, {
-        opacity: 1,
-        ...offset(10, 78),
-        duration: 0.5,
-      })
-      .to(textRef.current[0], { opacity: 1, y: "0", duration: 1 }, "<")
-      .to(pointerRef.current, { scale: 0.85, duration: 0.5 })
-      .to(pointerRef.current, { scale: 1, duration: 0.5 })
-      .to(hokkaidoRef.current, { opacity: 1, scale: 1, duration: 0.25 }, "<")
-      .to(
-        pointerRef.current,
-        {
-          ...offset(82, 16),
-          duration: 2,
-        },
-        "<"
-      )
-      .to(pointerRef.current, { scale: 0.85, duration: 0.5 })
-      .to(pointerRef.current, { scale: 1, duration: 0.5 })
-      .to(suzumeRef.current, { opacity: 1, scale: 1, duration: 0.25 }, "<")
-      .addLabel("Add Place End")
-      .to(textRef.current[0], { opacity: 0, duration: 0.5 })
-      .set(collabPointerRef.current, { opacity: 0 })
-      .fromTo(
-        collabPointerRef.current,
-        { opacity: 0.2, ...offset(40, 20) },
-        { opacity: 1, ...offset(63, 61), duration: 3, ease: "power3.out" }
-      )
-      .to(textRef.current[1], { opacity: 1, y: "0", duration: 1 }, "<")
-      .to(collabPointerRef.current, { scale: 0.85, duration: 0.5 })
-      .to(collabPointerRef.current, { scale: 1, duration: 0.5 })
-      .to(tokyoRef.current, { opacity: 1, scale: 1, duration: 0.25 }, "<")
-      .to(pointerRef.current, { ...offset(71, 30.5), duration: 1 }, "<")
-      .to(pointerRef.current, { scale: 0.85, duration: 0.5 })
-      .to(pointerRef.current, { scale: 1, duration: 0.5 })
-      .to(spRef.current, { opacity: 1, scale: 1, duration: 0.25 }, "<")
-      .addLabel("Collab End")
-      .to(textRef.current[1], { opacity: 0, duration: 0.5 })
-      .fromTo(
-        trivialPathRef.current[0],
-        { ...revealSegmentByLength(trivialPathRef.current[0]) },
-        { strokeDashoffset: 0, duration: 2, delay: 0.02 }
-      )
-      .to(textRef.current[2], { opacity: 1, y: "0", duration: 1 }, "<")
-      .fromTo(
-        trivialPathRef.current[1],
-        { ...revealSegmentByLength(trivialPathRef.current[1]) },
-        { strokeDashoffset: 0, duration: 1 }
-      )
+      tl.set(collabPointerRef.current, { opacity: 0 })
+        .to(pointerRef.current, {
+          opacity: 1,
+          ...offset(10, 78),
+          duration: 0.5,
+        })
+        .to(textRef.current[0], { opacity: 1, y: "0", duration: 1 }, "<")
+        .to(pointerRef.current, { scale: 0.85, duration: 0.5 })
+        .to(pointerRef.current, { scale: 1, duration: 0.5 })
+        .to(hokkaidoRef.current, { opacity: 1, scale: 1, duration: 0.25 }, "<")
+        .to(
+          pointerRef.current,
+          {
+            ...offset(82, 16),
+            duration: 2,
+          },
+          "<"
+        )
+        .to(pointerRef.current, { scale: 0.85, duration: 0.5 })
+        .to(pointerRef.current, { scale: 1, duration: 0.5 })
+        .to(suzumeRef.current, { opacity: 1, scale: 1, duration: 0.25 }, "<")
+        .addLabel("Add Place End")
+        .to(textRef.current[0], { opacity: 0, duration: 0.5 })
+        .set(collabPointerRef.current, { opacity: 0 })
+        .fromTo(
+          collabPointerRef.current,
+          { opacity: 0.2, ...offset(40, 20) },
+          { opacity: 1, ...offset(63, 61), duration: 3, ease: "power3.out" }
+        )
+        .to(textRef.current[1], { opacity: 1, y: "0", duration: 1 }, "<")
+        .to(collabPointerRef.current, { scale: 0.85, duration: 0.5 })
+        .to(collabPointerRef.current, { scale: 1, duration: 0.5 })
+        .to(tokyoRef.current, { opacity: 1, scale: 1, duration: 0.25 }, "<")
+        .to(pointerRef.current, { ...offset(71, 30.5), duration: 1 }, "<")
+        .to(pointerRef.current, { scale: 0.85, duration: 0.5 })
+        .to(pointerRef.current, { scale: 1, duration: 0.5 })
+        .to(spRef.current, { opacity: 1, scale: 1, duration: 0.25 }, "<")
+        .addLabel("Collab End")
+        .to(textRef.current[1], { opacity: 0, duration: 0.5 })
+        .fromTo(
+          trivialPathRef.current[0],
+          { ...revealSegmentByLength(trivialPathRef.current[0]) },
+          { strokeDashoffset: 0, duration: 2, delay: 0.02 }
+        )
+        .to(textRef.current[2], { opacity: 1, y: "0", duration: 1 }, "<")
+        .fromTo(
+          trivialPathRef.current[1],
+          { ...revealSegmentByLength(trivialPathRef.current[1]) },
+          { strokeDashoffset: 0, duration: 1 }
+        )
+        .fromTo(
+          trivialPathRef.current[2],
+          { ...revealSegmentByLength(trivialPathRef.current[2]) },
+          { strokeDashoffset: 0, duration: 1 }
+        )
+        .addLabel("Add Path End")
+        .to(textRef.current[2], { opacity: 0, duration: 0.5 })
+        .to(textRef.current[3], { opacity: 1, y: "0", duration: 1 })
+        .fromTo(
+          wandRef.current,
+          { opacity: 0, ...offset(45, 35) },
+          { opacity: 1, ...offset(49, 43), duration: 1 }
+        )
+        .to(wandRef.current, { scale: 0.85, duration: 0.5 })
+        .to(wandRef.current, { scale: 1, duration: 0.5 })
+        .to(trivialPathRef.current, { opacity: 0, duration: 0.5 }, "<")
+        .to(pointerRef.current, { opacity: 0, duration: 0.5 }, "<")
+        .to(collabPointerRef.current, { opacity: 0, duration: 0.5 }, "<")
+        .to(wandRef.current, { opacity: 0, duration: 0.5 })
+        .to(optPathRef.current, { opacity: 1, y: 0, duration: 0.5 }, "<")
+        .addLabel("Finished")
+        .to({}, { duration: 1 });
 
-      .fromTo(
-        trivialPathRef.current[2],
-        { ...revealSegmentByLength(trivialPathRef.current[2]) },
-        { strokeDashoffset: 0, duration: 1 }
-      )
-      .addLabel("Add Path End")
-      .to(textRef.current[2], { opacity: 0, duration: 0.5 })
-      .to(textRef.current[3], { opacity: 1, y: "0", duration: 1 })
-      .fromTo(
-        wandRef.current,
-        { opacity: 0, ...offset(45, 35) },
-        { opacity: 1, ...offset(49, 43), duration: 1 }
-      )
-      .to(wandRef.current, { scale: 0.85, duration: 0.5 })
-      .to(wandRef.current, { scale: 1, duration: 0.5 })
-      .to(trivialPathRef.current, { opacity: 0, duration: 0.5 }, "<")
-      .to(pointerRef.current, { opacity: 0, duration: 0.5 }, "<")
-      .to(collabPointerRef.current, { opacity: 0, duration: 0.5 }, "<")
-      .to(wandRef.current, { opacity: 0, duration: 0.5 })
-      .to(optPathRef.current, { opacity: 1, y: 0, duration: 0.5 }, "<")
-      .addLabel("Finished");
-
+    }, containerRef);
     return () => {
-      tl.current?.kill();
-      ScrollTrigger.killAll();
+      ctx.revert();
     };
   }, []);
 
@@ -175,9 +176,9 @@ const Description: React.FC = () => {
         lg:text-5xl lg:left-[-35%] lg:w-[90%] lg:top-[30%] lg:leading-tight lg:max-w-[50%] lg:text-center
         text-pretty text-2xl top-[20%] left-[15%] w-[35%] text-center font-semibold"
         >
-          <div className="relative">
+          <div className="relative drop-shadow-md">
             <div
-              className="absolute top-0 left-0 w-full opacity-0 transform translate-y-[30%]"
+              className="absolute top-0 left-0 w-full opacity-0 transform translate-y-[30%] "
               ref={(el) => (textRef.current[0] = el)}
             >
               Add places from guides with 1 click
