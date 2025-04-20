62dcabb06df76e704e0e7230acc8f7098f67e164
Johnson Wang <jw2693@cornell.edu>
2025-04-19 20:31:55 -0400

    removed unused components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import Description from "@components/pages/description";
-import AnimatedButton from "@components/reusable/animatedButton";
-import MeshGradientBackground from "@components/reusable/meshGradientBackground";
 import Login from "./login/page";
 
 export default function Home() {
