5de76b4f61ca854c3cdbd397bd7dcb384a53c592
Johnson Wang <jw2693@cornell.edu>
2025-06-04 15:31:28 -0400

    configured oauth for local development

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -7,7 +7,7 @@ export async function updateSession(request: NextRequest) {
   })
 
   const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    "http://127.0.0.1:54321",
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -5,7 +5,7 @@ export async function createClient() {
   const cookieStore = await cookies()
 
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    "http://127.0.0.1:54321",
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
